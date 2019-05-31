65ae31dda3eee074c795f4db3f6eec3ff3f49003
josephriosio <blankworldwide@gmail.com>
2019-05-31 15:38:21 -0700

    connect back-end to front-end

--- quizzer/src/App.js
+++ quizzer/src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  // Declare a new state variable, which we'll call "count"
-  const [name, setName] = useState("Quizzer App");
+  const [name] = useState("Quizzer App");
+  const [users, setUsers] = useState([]);
+
+  //takes place instead of componentDidMount
+  useEffect(async () => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://labs13-quizzer.herokuapp.com/api/users"
+      );
+      //setting database data to state with hooks
+      setUsers(result.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <p>{name}</p>
+      {users.map(user => (
+        <li key={user.id}>
+          <p>{user.name}</p>
+        </li>
+      ))}
     </div>
   );
 }
