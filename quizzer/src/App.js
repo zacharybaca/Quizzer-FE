c648aec13c711e062129806bf3be6c0c6156935c
josephriosio <blankworldwide@gmail.com>
2019-05-31 13:55:34 -0700

    added name

--- quizzer/src/App.js
+++ quizzer/src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [name, setName] = useState("Quizzer App");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{name}</p>
     </div>
   );
 }
