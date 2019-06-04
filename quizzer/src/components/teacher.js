900a6689772c432f2aea0cfd59ab8b5c8f895020
Marc Thiongo <marcthuo@gmail.com>
2019-06-04 13:30:41 -0400

    set up of teacher, students and re-edit users

---
+++ quizzer/src/components/teacher.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from 'styled-components';
+
+const TeacherPage = styled.div`
+  text-align: center;
+`;
+
+function Teacher() {
+    const [name] = useState("Teachers");
+    const [teachers, setTeachers] = useState([]);
+  
+    //takes place instead of componentDidMount
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios(
+          "https://labs13-quizzer.herokuapp.com/api/profile/teacher"
+        );
+        //setting database data to state with hooks
+        setTeachers(result.data);
+      };
+      fetchData();
+    }, []);
+    
+    return (
+        <TeacherPage>
+          <p>{name}</p>
+    
+          {teachers.map(user => (
+            <li key={user.id}>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+              <p>Username: {user.username}</p>
+              <p>Account: {user.role}</p>
+            </li>
+          ))}
+        </TeacherPage>
+    );
+}
+
+export default Teacher;
