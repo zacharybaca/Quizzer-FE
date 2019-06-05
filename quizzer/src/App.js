eda7569cab5e8001f6d584c9cbf5177b92b6fcb9
Rosa Perez <rosaperez@Rosas-MBP.attlocal.net>
2019-06-05 16:21:34 -0700

    adding plans

--- quizzer/src/App.js
+++ quizzer/src/App.js
@@ -11,7 +11,7 @@ import Student from './components/student';
 import Teacher from './components/teacher';
 
 import StripePage from '../src/components/StripePage/StripePage'
-// import Step2Page from './components/Step2/Step2Page';
+import Step2Page from './components/Step2/Step2Page';
 
 
 const Homepage = styled.div`
@@ -61,7 +61,7 @@ function App () {
 
         {/* <Route extact path="/" component={Home}/> */}
         <Route extact path='/step1' component={StripePage}/>
-        {/* <Route extact path='/step2' component={Step2Page}/> */}
+        <Route extact path='/step2' component={Step2Page}/>
 
 
 
--- quizzer/src/components/Step2/Step2Page.js
+++ quizzer/src/components/Step2/Step2Page.js
@@ -1,31 +1,75 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// // this is where customer selects a Plan
+// this is where customer selects a Plan
 
-// class Step2Page extends Component {
-//     constructor(props) {
-//         super(props);
+class Step2Page extends Component {
+    constructor(props) {
+        super(props);
 
-//         this.state = {
-//             coupon: '',
-//             currentPlan: 'bronze',
-//         }
-//     }
+        this.state = {
+            coupon: '',
+            currentPlan: 'bronze',
+        };
+    }
 
-//     render() {
-//         <div>
-//             <div>
-//                 <input
-//                 type="text"
-//                 placeholder="Coupon"
-//                 value={}
-//                 />
-//             </div>
-//         </div>
-//         return (
-//             <p>Hello World</p>
-//         )
-//     }
-// }
+        this.onCouponChange = this.onCouponChange.bind(this);
+    }
 
-// export default Step2Page;
+    onCouponChange(event) {
+        this.setState({
+            coupon: event.target.value,
+        });
+    }
+    
+    render() {
+        const {
+            coupon,
+            currentPlan,
+        } = this.state;
+
+        const plans = ['bronze', 'silver', 'gold'];
+
+        return (
+        <div>
+            <div>
+                <input
+                type="text"
+                placeholder="Coupon"
+                value={coupon}
+                onChange={this.onCouponChange}
+                />
+            </div>
+            <div>
+                <h2>Plans</h2>
+                {
+                    (plans).map((plan) => {
+                        if (currentPlan == plan) {
+                        return (
+                            <button
+                               style={{
+                                backgroundColor: '#d8d8d8',
+                            }}
+                            >
+                                {plan}
+                                </button>
+                        )
+                      }
+                      return (
+                        <button
+                           style={{
+                            backgroundColor: '#ff',
+                        }}
+                        >
+                            {plan}
+                            </button>
+                     )
+                    })
+                }
+                            </div>
+                            </div>
+
+        );
+    }
+}
+
+export default Step2Page;
