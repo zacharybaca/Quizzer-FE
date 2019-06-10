16000d9e851f9435f946eee1c48aa96ab158bd7f
Rosa Perez <rosaperez@Rosas-MBP.attlocal.net>
2019-06-10 16:48:37 -0700

    adding initial Quiz component

---
+++ quizzer/src/components/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Quiz extends React.Component {
+      state = {
+        userAnswer: null,
+        currentQuestion: 0,
+        options: []
+      }
+  }
+
+export default MainQuiz;
