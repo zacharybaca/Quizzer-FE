f3ecf8527925e30c991e00e7efaa6a280d87b244
josephriosio <blankworldwide@gmail.com>
2019-06-12 16:49:03 -0700

    added teacher dashboard barebones

--- quizzer/src/components/student.js
+++ quizzer/src/components/student.js
@@ -1,41 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StudentPage = styled.div`
   text-align: center;
 `;
 
 function Student() {
-    const [name] = useState("Students");
-    const [students, setStudents] = useState([]);
-  
-    //takes place instead of componentDidMount
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios(
-          "https://labs13-quizzer.herokuapp.com/api/profile/student"
-        );
-        //setting database data to state with hooks
-        setStudents(result.data);
-      };
-      fetchData();
-    }, []);
+  const [name] = useState("Students");
+  const [students, setStudents] = useState([]);
 
-    return (
-        <StudentPage>
-          <p>{name}</p>
-    
-          {students.map(user => (
-            <li key={user.id}>
-              <p>Name: {user.name}</p>
-              <p>Email: {user.email}</p>
-              <p>Username: {user.username}</p>
-              <p>Account: {user.role}</p>
-            </li>
-          ))}
-        </StudentPage>
-    );
+  //takes place instead of componentDidMount
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://labs13-quizzer.herokuapp.com/api/profile/student"
+      );
+      //setting database data to state with hooks
+      console.log("ran");
+      setStudents(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <StudentPage>
+      <p>{name}</p>
+      {console.log("ran")}
+      {students.map(user => (
+        <li key={user.id}>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Username: {user.username}</p>
+          <p>Account: {user.role}</p>
+        </li>
+      ))}
+    </StudentPage>
+  );
 }
 
 export default Student;
