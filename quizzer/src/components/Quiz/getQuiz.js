55f386b7e4927fe5407dd1da6d866d62fc467f8f
Rosa Perez <rosaperez@Rosas-MBP.attlocal.net>
2019-06-12 19:07:03 -0700

    api get quizzes (work in progress)

--- quizzer/src/components/Quiz/getQuiz.js
+++ quizzer/src/components/Quiz/getQuiz.js
@@ -6,22 +6,16 @@ import axios from 'axios';
 class getQuiz extends Component {
   componentDidMount() {
     axios
-    .get('https://labs13-quizzer.herokuapp.com/api/quest/question')
+    .get('https://labs13-quizzer.herokuapp.com/api/quiz/quizzes')
     .then(response => console.log(response))
     .catch(err => console.log(err));
-//     console.log("inside CDM");
-//     fetch('https://labs13-quizzer.herokuapp.com/api/quest/question')
-//     .then(res => res.json())
-//     .then(dogs => console.log("fetch completed"))
-//     .catch(err => console.log(err));
-//   console.log("below fetch");
   }
 
   render() {
     return (
       <div className="App">
       <h1>Student Get Quizzes</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {/* <h2>Start editing to see some magic happen!</h2> */}
     </div>
     );
   }
