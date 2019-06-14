f39465ffdb0c503559687388857f56d66411bf2d
cubsfan46350 <cubsfan.46350@msn.com>
2019-06-13 19:14:35 -0500

    changed add quiz to class component

--- quizzer/src/components/QuizForm/AddQuiz.js
+++ quizzer/src/components/QuizForm/AddQuiz.js
@@ -1,35 +1,53 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function AddQuiz() {
-    const [quiz, setQuiz] = useState({
-        "id": null,
-        "category": "",
-        "type": "",
-        "difficulty": "",
-        "question": "",
-        "correct_answer": "",
-        "incorrect_answers": "",
-        "points": null,
-        "options": [],
-        "answer": "",
-        "questions": ""
-    })
+class AddQuiz extends React.Component {
+    state = {
+        quiz_name: '',
+        quiz_description: ''
+    }
+
+    handleChange = event => {
+        this.setState({ quiz_name: event.target.value });
+        this.setState({ quiz_description: event.target.value});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const quiz = {
+            quiz_name: this.state.quiz_name,
+            quiz_description: this.state.quiz_description
+        };
+
+        axios.post(`https://labs13-quizzer.herokuapp.com/api/quiz/quizzes`, { quiz })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
+
+        
+      
+render () {
     return (
         <div>
             <div className="quiz-name-section">
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label for="quiz-name">Quiz Name</label>
                     <br />
-                    <input className="text-box" type="text" name="quiz" id="quiz-name" />
+                    <input className="text-box" type="text" value={this.state.quiz_name} onChange={this.handleChange} />
                     <br />
                     <br />
                     <label className="add-quiz-label" for="quiz-description">Add Quiz Description</label>
                     <br />
-                    <input className="add-quiz-text-box" type="text" name="quiz" id="quiz-description" />
+                    <input className="add-quiz-text-box" type="text" value={this.state.quiz_description} onChange={this.handleChange} />
+                    <br />
+                    <button type="submit">Add Quiz</button>
                 </form>
             </div>
         </div>
     )
-}
+}}
+
 
 export default AddQuiz;
