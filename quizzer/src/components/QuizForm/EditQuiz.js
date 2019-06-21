d9b83a47e622d6238249421de1fe5f6b8a1fa52d
Marc Thiongo <marcthuo@gmail.com>
2019-06-21 11:47:55 -0400

    changes to edt quiz

--- quizzer/src/components/QuizForm/EditQuiz.js
+++ quizzer/src/components/QuizForm/EditQuiz.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import TeacherNavigation from "../Dashboards/Navigation/TeacherNavigation";
 import axios from "axios";
+import "./EditQuiz.css";
 
 const EditQuiz = props => {
   const [componentData, setComponentData] = useState({
@@ -25,16 +26,30 @@ const EditQuiz = props => {
     fetchData();
   }, []);
 
+  const updateQuiz = async () => {
+    const res = await axios.delete(
+      `${process.env.REACT_APP_BE_URL || process.env.REACT_APP_BE_LOCAL}/api/quiz/quizzes/${quizId}`
+    );
+  };
+
   const deleteQuiz = async () => {
     const res = await axios.delete(
       `${process.env.REACT_APP_BE_URL || process.env.REACT_APP_BE_LOCAL}/api/quiz/quizzes/${quizId}`
     );
   };
+
   return (
     <>
       <TeacherNavigation />
-      <div>hello</div>
-      <button onClick={deleteQuiz}>delete quiz</button>
+      <div className = "main">
+        <div className = "choices">
+          <div>Please select one of the following choices</div>
+          <div>
+            <button className='button' onClick={updateQuiz}>update quiz</button>
+            <button className='button' onClick={deleteQuiz}>delete quiz</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
