74a726347e86d7536950a4884eb35d03eed0cc9d
josephriosio <blankworldwide@gmail.com>
2019-06-24 15:17:50 -0700

    basic folder adding system

--- quizzer/src/components/InfoComponents/Folders.js
+++ quizzer/src/components/InfoComponents/Folders.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {
   ButtonDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Button,
   Modal,
   ModalHeader,
-  ModalBody,
-  ModalFooter
+  ModalBody
 } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
@@ -17,58 +15,58 @@ import { faFolder } from "@fortawesome/free-solid-svg-icons";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import "./folders.css";
 
-class Folders extends Component {
-  constructor(props) {
-    super(props);
+function Folders(props) {
+  // const [reactstrap, setReactStrap] = useState({
+  //   dropdownOpen: false,
+  //   modal: false
+  // });
+  const [modal, setModal] = useState(false);
+  const [dropdownOpen, setDropDownOpen] = useState(false);
+  const [folderHolder, setFolders] = useState({
+    folderName: "",
+    folders: []
+  });
 
-    this.toggle = this.toggle.bind(this);
-    this.togglepopup = this.togglepopup.bind(this);
-
-    this.state = {
-      dropdownOpen: false,
-      modal: false,
-      folderName: "",
-      folders: []
-    };
-  }
-  componentDidMount() {
-    axios
-      .get(
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
         `${process.env.REACT_APP_BE_URL ||
           process.env.REACT_APP_BE_LOCAL}/api/folder/${localStorage.getItem(
           "id"
         )}`
-      )
-      .then(response =>
-        this.setState({
-          folders: response.data
-        })
-      )
-      .catch(err => console.log(err));
-  }
+      );
+      //setting database data to state with hooks
+      console.log(result.data);
+      setFolders({ folders: result.data });
+      console.log(folderHolder);
+    };
+    fetchData();
+  }, [setFolders]);
 
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
+  // const toggle = () => {
+  //   setReactStrap({
+  //     ...reactstrap,
+  //     dropdownOpen: !dropdownOpen
+  //   });
+  // };
+  //
+  // const togglepopup = () => {
+  //   setReactStrap({
+  //     ...reactstrap,
+  //     modal: !modal
+  //   });
+  // };
 
-  togglepopup() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
+  const onChanges = event =>
+    setFolders({ ...folderHolder, [event.target.name]: event.target.value });
 
-  onChanges = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state.folderName);
+    const { folderName } = folderHolder;
+
     const folder = {
       teacher_id: localStorage.getItem("id"),
-      folder_name: this.state.folderName
+      folder_name: folderName
     };
     console.log(folder);
     axios
@@ -81,73 +79,78 @@ class Folders extends Component {
         console.log(res);
         console.log(res.data);
       });
-    this.setState({
-      folderName: "",
-      modal: false
-    });
+
+    setModal(!modal);
   };
 
-  render() {
-    const { folders } = this.state;
-    return (
-      <div className="sidebar">
-        <div>
-          <ButtonDropdown
-            direction="right"
-            isOpen={this.state.btnDropright}
-            toggle={() => {
-              this.setState({ btnDropright: !this.state.btnDropright });
-            }}
-          >
-            <DropdownToggle caret>+ NEW</DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem>
-                <Link to="/quizzes">New Quiz</Link>
-              </DropdownItem>
-              <DropdownItem onClick={this.togglepopup}>New Folder</DropdownItem>
-              <Modal isOpen={this.state.modal} togglepopup={this.togglepopup}>
-                <ModalHeader togglepopup={this.togglepopup}>
-                  Add Folder
-                </ModalHeader>
+  const { folders } = folderHolder;
+
+  return (
+    <div className="sidebar">
+      <div>
+        <ButtonDropdown
+          direction="right"
+          isOpen={dropdownOpen}
+          toggle={() => {
+            setDropDownOpen(!dropdownOpen);
+          }}
+        >
+          <DropdownToggle caret>+ NEW</DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>
+              <Link to="/quizzes">New Quiz</Link>
+            </DropdownItem>
+            <DropdownItem
+              onClick={() => {
+                setModal(!modal);
+              }}
+            >
+              New Folder
+              <Modal isOpen={modal}>
+                <ModalHeader>Add Folder</ModalHeader>
                 <ModalBody>
-                  <form onSubmit={this.handleSubmit}>
+                  <form onSubmit={handleSubmit}>
                     <input
                       name="folderName"
                       placeholder="enter folder name"
                       type="text"
-                      value={this.state.folderName}
-                      onChange={this.onChanges}
+                      value={folderHolder.folderName}
+                      onChange={e => onChanges(e)}
                     />
 
                     <button type="submit">Create</button>
-                    <button onClick={this.togglepopup}>Cancel</button>
+                    <button
+                      onClick={() => {
+                        setModal(!modal);
+                      }}
+                    >
+                      Cancel
+                    </button>
                   </form>
                 </ModalBody>
               </Modal>
-              <DropdownItem>Get access code</DropdownItem>
-            </DropdownMenu>
-          </ButtonDropdown>
-        </div>
+            </DropdownItem>
+            <DropdownItem>Get access code</DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+      </div>
+      <div>
         <div>
-          <div>
-            {console.log(folders)}
-            {folders.length > 0 ? (
-              folders.map(folder => (
-                <div>
-                  <FontAwesomeIcon icon={faAngleRight} />
-                  <FontAwesomeIcon icon={faFolder}>
-                    {folder.folder_name}
-                  </FontAwesomeIcon>
-                </div>
-              ))
-            ) : (
-              <div>hi</div>
-            )}
-          </div>
+          {console.log(folders)}
+          {folders.length > 0 ? (
+            folders.map(folder => (
+              <div key={folder.id}>
+                <FontAwesomeIcon icon={faAngleRight} />
+                <FontAwesomeIcon icon={faFolder} /> {folder.folder_name}
+              </div>
+            ))
+          ) : (
+            <div>hi</div>
+          )}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Folders;
