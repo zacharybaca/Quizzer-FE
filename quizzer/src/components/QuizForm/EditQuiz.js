6cd39baabb01f0db5ba656e96a63daf19244420d
josephriosio <blankworldwide@gmail.com>
2019-06-26 15:58:10 -0700

    added question edit

--- quizzer/src/components/QuizForm/EditQuiz.js
+++ quizzer/src/components/QuizForm/EditQuiz.js
@@ -15,7 +15,21 @@ const EditQuiz = props => {
     description: ""
   });
 
-  const { quizId, data } = componentData;
+  const [questionInfo, setQuestionInfo] = useState([]);
+  const [Eachquestion, setQuestion] = useState({
+    category: "Math",
+    type: 1,
+    Q_content: "",
+    A: "",
+    B: "",
+    C: "",
+    D: "",
+    correct_answer: "",
+    points: "",
+    id: ""
+  });
+
+  const { quizId, data, questionId } = componentData;
   const { quiz_name, description } = quizInfo;
 
   //takes place instead of componentDidMount
@@ -28,6 +42,7 @@ const EditQuiz = props => {
           process.env.REACT_APP_BE_LOCAL}/api/quiz/quizzes/${id}`
       );
       //setting database data to state with hooks
+      console.log(res.data);
 
       // useState
       setComponentData({
@@ -41,6 +56,21 @@ const EditQuiz = props => {
         quiz_name: res.data.quiz[0].quiz_name,
         description: res.data.quiz[0].description
       });
+      setQuestionInfo(res.data.quiz);
+      setQuestion(
+        res.data.quiz.map(question => ({
+          category: question.category,
+          type: question.type,
+          Q_content: question.Q_content,
+          A: question.A,
+          B: question.B,
+          C: question.C,
+          D: question.D,
+          correct_answer: question.correct_answer,
+          points: question.points,
+          id: question.id
+        }))
+      );
     };
     fetchData();
   }, []);
@@ -54,6 +84,9 @@ const EditQuiz = props => {
 
   const onChange = e =>
     setQuizInfo({ ...quizInfo, [e.target.name]: e.target.value });
+
+  const handleChanges = e =>
+    setQuestion({ ...Eachquestion, [e.target.name]: e.target.value });
 
   const deleteQuiz = async () => {
     const res = await axios.delete(
@@ -80,6 +113,22 @@ const EditQuiz = props => {
     props.history.push("/teachersDashboard");
   };
 
+  const handleSubmit = async (e, id) => {
+    e.preventDefault();
+
+    const res = await axios.put(
+      `${process.env.REACT_APP_BE_URL ||
+        process.env.REACT_APP_BE_LOCAL}/api/quest/questions/${id}`,
+      questionInfo
+    );
+
+    console.log(res);
+    props.history.push("/teachersDashboard");
+  };
+
+  console.log(questionInfo);
+  console.log("data", Eachquestion);
+
   return (
     <>
       <TeacherNavigation />
@@ -102,6 +151,120 @@ const EditQuiz = props => {
               update quiz
             </button>
           </form>
+          {console.log(Eachquestion, questionInfo)}
+          {Eachquestion.length > 0
+            ? Eachquestion.map(question => (
+                <form onSubmit={e => handleSubmit(e, question.id)}>
+                  <label>Category</label>
+                  <br />
+                  <select
+                    value={question.category}
+                    className="text-box"
+                    name="category"
+                    onChange={handleChanges}
+                  >
+                    <option value="Math">Math</option>
+                    <option value="Science">Science</option>
+                    <option value="English">English</option>
+                    <option value="History">History</option>
+                    <option value="Spanish">Spanish</option>
+                  </select>
+                  <br />
+                  <br />
+                  <label>Type</label>
+                  <br />
+                  <select
+                    onChange={handleChanges}
+                    value={question.type}
+                    className="text-box"
+                    name="type"
+                  >
+                    <option value={1}>Standard</option>
+                    <option value={2}>Remedial</option>
+                  </select>
+
+                  <br />
+                  <br />
+                  <label>Question</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="Q_content"
+                    className="text-box"
+                    type="text"
+                    value={question.Q_content}
+                  />
+                  <br />
+                  <br />
+                  <label>A</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="A"
+                    className="text-box"
+                    type="text"
+                    value={question.A}
+                  />
+                  <br />
+                  <br />
+                  <label>B</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="B"
+                    className="text-box"
+                    type="text"
+                    value={question.B}
+                  />
+                  <br />
+                  <br />
+                  <label>C</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="C"
+                    className="text-box"
+                    type="text"
+                    value={question.C}
+                  />
+                  <br />
+                  <br />
+                  <label>D</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="D"
+                    className="text-box"
+                    type="text"
+                    value={question.D}
+                  />
+                  <br />
+                  <br />
+                  <label>Correct Answer</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="correct_answer"
+                    className="text-box"
+                    type="text"
+                    value={question.correct_answer}
+                  />
+                  <br />
+                  <br />
+                  <label>Points</label>
+                  <br />
+                  <input
+                    onChange={handleChanges}
+                    name="points"
+                    className="text-box"
+                    type="text"
+                    value={question.points}
+                  />
+                  <br />
+                  <button type="submit">update Question</button>
+                </form>
+              ))
+            : null}
 
           <div>
             <button className="button" onClick={deleteQuiz}>
